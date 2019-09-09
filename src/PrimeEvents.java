ba8ef7b3995ea241aca472aa6de6aa9a505e5091
Ganesh Sarma K <gkan0007@student.monash.edu>
2019-09-09 12:45:44 +1000

    First commit

---
+++ src/PrimeEvents.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class PrimeEvents {
+
+    PrimeEvents()
+    {
+    }
+
+
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("1.login");
+         System.out.println("2.register");
+         int choice = scanner.nextInt();
+     }
+}
