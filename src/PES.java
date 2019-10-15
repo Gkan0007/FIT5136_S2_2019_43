bdec5bcc308a478db57e7e3b05f212579c3f69e9
Fanao Fan <ffan0005@student.monash.edu>
2019-10-15 20:45:56 +1100

    1

---
+++ src/PES.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class PES {
+    private ArrayList<HallList> listOfHalls;
+    private ArrayList<BookingList> istOfBookings;
+    private ArrayList<QuotationList> listOfQuotations;
+    private ArrayList<UserList> listOfUsers;
+
+    public boolean login(){return true; }
+    public boolean register(){return true;}
+    public void customerPortal(String cPortal){}
+    public void ownerPortal( String oPortal) {}
+    public void adminPortal (String aPortal) {}
+    public String isCustomerOrOwner(String checkUser) {return }
+
+
+}
+
---
+++ src/UserInterface.java
@@ -0,0 +1,22 @@
+public class UserInterface {
+    public void displayMenu(Customer customer) { }
+    public void displayMenu(Owner owner) { }
+    public void displayMenu(Admin admin) { }
+    public void display(Hall hall) {}
+    public void display(Hall[] hallList) {}
+    public void display(Quotation quotation) {}
+    public void display(Quotation[] quotationList) {}
+    public void display(Booking booking) {}
+    public void display(Booking[] bookingList) {}
+    public void displayMenu(Hall hall) {}
+    public void displayMenu(Quotation quotation){}
+    public void displayMenu(Booking booking){}
+    public void displayReviews(Hall review){}
+    public boolean askForConformation() { return true;}
+    public void promptInputMessage(String message) {}
+    public boolean  displayExitScreen() {return true;}
+    public void displayError(String error) {}
+
+
+
+}
